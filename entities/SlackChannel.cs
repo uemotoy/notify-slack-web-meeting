fbb14863b2c753a398ce919509a712f62c49f70f
uemoto_y <yusuke.uemoto.j8y@jpgr.denso.com>
2021-11-30 21:44:55 +0900

    SlackChannelのJSONPropertyを設定

--- entities/SlackChannel.cs
+++ entities/SlackChannel.cs
@@ -1,4 +1,5 @@
 using System;
+using Newtonsoft.Json;
 
 namespace dcinc.api.entities
 {
@@ -19,22 +20,27 @@ namespace dcinc.api.entities
     /// <summary>
     /// 一意とするID
     /// </summary>
+    [JsonProperty("id")]
     public string Id { get; set; }
     /// <summary>
     /// Slackチャンネル名
     /// </summary>
+    [JsonProperty("name")]
     public string Name { get; set; }
     /// <summary>
     /// SlackチャンネルのWebhook URL
-    /// </summary>
+    /// </summary>   
+    [JsonProperty("webhookUrl")]
     public string WebhookUrl { get; set; }
     /// <summary>
     /// 登録者
     /// </summary>
+    [JsonProperty("registeredNy")]
     public string RegisteredBy { get; set; }
     /// <summary>
     /// 登録日時（UTC）
     /// </summary>
+    [JsonProperty("registeredAt")]
     public DateTime RegisteredAt { get; set; }
 
     #endregion
